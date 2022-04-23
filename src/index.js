36f4ddfa5fa35adfe3e6d454a1af2e6e806ca351
MarioBasCo <mario-basilio1998@hotmail.com>
2022-04-23 16:11:55 -0500

    app completa

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ app.set('port', process.env.PORT || 4000);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use('/', express.static(path.join(__dirname + '/public')));
 
-
-app.use("/images", express.static(path.join(__dirname+ '/uploads')));
+app.use("/images", express.static(path.join(__dirname + '/uploads')));
 
 
 //Rutas
